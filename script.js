62dd982a3f44b862c67f24b0515bf934f9c9dcf9
sirajju <84080512+sirajju@users.noreply.github.com>
2023-07-01 16:21:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,12 @@
     localStorage.setItem('view_count',count)
   }
   else{
-    delete count
     let digits = '123456789';
     let count =0
         for (let i = 0; i < 3; i++) {
             count += digits[Math.floor(Math.random() * 10)]
         }
     localStorage.setItem('view_count',count)
-  }
   container.innerHTML += count
+  }
+  
